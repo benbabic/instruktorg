ba68310fcd96c4e9bc61742c8624b081fd3d2465
Ben <ben@juliand.co.uk>
2017-03-03 11:59:19 +0000

    Add changeset to process
    It validates that the name is provided

--- web/models/process.ex
+++ web/models/process.ex
@@ -1,9 +1,17 @@
 defmodule Instruktorg.Process do
   use Ecto.Schema
 
+  import Ecto.Changeset
+
   schema "process" do
     field :name,     :string
     field :steps,    :map
     field :category, :string
+  end
+
+  def changeset(process, params \\ %{}) do
+    process
+      |> cast(params, [:name, :steps, :category])
+      |> validate_required([:name])
   end
 end
